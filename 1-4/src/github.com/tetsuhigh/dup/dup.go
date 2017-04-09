656f3748bd6c2eb5fc3c16e02034da152a496539
tetsuhigh <tetsuhighperformance@gmail.com>
2017-04-10 00:11:22 +0900

    dup2 標準入力か、名前が指定されたファイルの一覧から読み込むように変更

--- 1-4/src/github.com/tetsuhigh/dup/dup.go
+++ 1-4/src/github.com/tetsuhigh/dup/dup.go
@@ -6,10 +6,42 @@ import (
 	"os"
 )
 
+/*
+ Dup2 入力に2回以上現れた行の数とその行のテキストを表示する。
+ 標準入力から読み込むか、名前が指定されたファイルの一覧から読み込む。
+*/
 func main() {
-	fmt.Println("start")
 	// keyがstring、valueがintの空mapを生成する
 	counts := make(map[string]int)
+	files := os.Args[1:]
+	
+	if len(files) == 0 {
+		fmt.Println("inout start")
+		countLines(os.Stdin, counts)
+	} else {
+		// 第一戻り値はインデックスで"_"(アンダーバー)に代入することで破棄される
+		for _, arg := range files {
+			f, err := os.Open(arg)
+			if err != nil {
+				// 標準出力にエラー情報を出力
+				fmt.Fprintf(os.Stderr, "dup2: %v\n", err)
+				continue
+			}
+			countLines(f, counts)
+			f.Close()
+		}
+	}
+	
+	fmt.Println("dup result")
+	// rangeはkey, valueを返す。順番は不定
+	for line, n := range counts {
+		if n > 1 {
+			fmt.Printf("%d\t%s\n", n, line)
+		}
+	}
+}
+
+func countLines(f *os.File, counts map[string]int) {
 	input := bufio.NewScanner(os.Stdin)
 	
 	for input.Scan() {
@@ -17,13 +49,7 @@ func main() {
 		if input.Text() == "" {
 			break
 		}
+		// 参照渡しなのでmainで渡されたオブジェクトを直接変更する
 		counts[input.Text()]++
-	}
-	
-	// rangeはkey, valueを返す。順番は不定
-	for line, n := range counts {
-		if n > 1 {
-			fmt.Printf("%d\t%s\n", n, line)
-		}
 	}
 }
