cce01023b67c0b53fed0b3c0235b4b9bcb702ab0
tetsuhigh <tetsuhighperformance@gmail.com>
2017-04-10 00:36:15 +0900

    dup3 ファイルを一括で読み込むように変更

--- 1-4/src/github.com/tetsuhigh/dup/dup.go
+++ 1-4/src/github.com/tetsuhigh/dup/dup.go
@@ -1,55 +1,40 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
+	"io/ioutil"
 	"os"
+	"strings"
 )
 
 /*
- Dup2 入力に2回以上現れた行の数とその行のテキストを表示する。
- 標準入力から読み込むか、名前が指定されたファイルの一覧から読み込む。
+ Dup3 入力に2回以上現れた行の数とその行のテキストを表示する。
+ 、名前が指定されたファイルの一覧から読み込む。
 */
 func main() {
 	// keyがstring、valueがintの空mapを生成する
 	counts := make(map[string]int)
-	files := os.Args[1:]
-	
-	if len(files) == 0 {
-		fmt.Println("inout start")
-		countLines(os.Stdin, counts)
-	} else {
-		// 第一戻り値はインデックスで"_"(アンダーバー)に代入することで破棄される
-		for _, arg := range files {
-			f, err := os.Open(arg)
-			if err != nil {
-				// 標準出力にエラー情報を出力
-				fmt.Fprintf(os.Stderr, "dup2: %v\n", err)
-				continue
-			}
-			countLines(f, counts)
-			f.Close()
+
+	// 第一戻り値はインデックスで"_"(アンダーバー)に代入することで破棄される
+	for _, filename := range os.Args[1:] {
+		data, err := ioutil.ReadFile(filename)
+		if err != nil {
+			// 標準出力にエラー情報を出力
+			fmt.Fprintf(os.Stderr, "dup3: %v\n", err)
+			continue
+		}
+		
+		// 改行文字が\r\nではないと一致判定されない
+		for _, line := range strings.Split(string(data), "\r\n") {
+			counts[line]++		
 		}
 	}
-	
+
 	fmt.Println("dup result")
 	// rangeはkey, valueを返す。順番は不定
 	for line, n := range counts {
 		if n > 1 {
 			fmt.Printf("%d\t%s\n", n, line)
 		}
-	}
-}
-
-func countLines(f *os.File, counts map[string]int) {
-	input := bufio.NewScanner(os.Stdin)
-	
-	for input.Scan() {
-		// 空文字なら標準入力を終える。止め方わからない
-		if input.Text() == "" {
-			break
-		}
-		// 参照渡しなのでmainで渡されたオブジェクトを直接変更する
-		counts[input.Text()]++
 	}
 }
