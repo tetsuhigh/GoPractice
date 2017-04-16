1e2243b31cc3aac58b8f110feae27583a391570b
tetsuhigh <tetsuhighperformance@gmail.com>
2017-04-16 23:01:14 +0900

    練習問題1-6 線の色をランダムに変更するように変更

---
+++ 1-6/src/github.com/tetsuhigh/lissajous/lissajous.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"image"
+	"image/color"
+	"image/gif"
+	"io"
+	"math"
+	"math/rand"
+	"os"
+	"time"
+)
+
+// 先頭の色が初期背景色になる。
+var palette = []color.Color{
+	color.Black,
+	color.White,
+	color.RGBA{0xff, 0x00, 0x00, 0xff},
+	color.RGBA{0x00, 0xff, 0x00, 0xff},
+	color.RGBA{0x00, 0x00, 0xff, 0xff},
+}
+
+// 定数、パッケージ全体で見える
+const (
+	whiteIndex = 1 // 白のバレットでの位置
+	blackIndex = 0 // 黒のバレットでの位置
+	redIndex   = 2 // 赤のバレットでの位置
+	greenIndex = 3 // 緑のバレットでの位置
+	blueIndex  = 4 // 青のバレットでの位置
+)
+
+/*
+  ランダムなリサージュ図形のGIFアニメーションを生成します。
+ "lissajous > out.gif"とすることで、out.gifに出力することができる
+*/
+func main() {
+	rand.Seed(time.Now().UTC().UnixNano())
+	lissajous(os.Stdout)
+}
+
+func lissajous(out io.Writer) {
+	// 定数、関数内のみで見える
+	const (
+		cycles  = 5     // 発振器xが完了する周回の回数
+		res     = 0.001 // 回転の分解能
+		size    = 100   // 画像キャンパスは[-size..+size]の範囲を扱う
+		nframes = 64    //アニメーションフレーム数
+		delay   = 8     // 10ms単位でのフレーム間の遅延
+	)
+
+	freq := rand.Float64() * 3.0 // 発振器yの相対周波
+	anim := gif.GIF{LoopCount: nframes}
+	phase := 0.0 // 位相差
+	rand.Seed(time.Now().UnixNano())
+	for i := 0; i < nframes; i++ {
+		// 201x201のpaletteの先頭の色で塗りつぶされた画像を生成する
+		rect := image.Rect(0, 0, 2*size+1, 2*size+1)
+		img := image.NewPaletted(rect, palette)
+		for t := 0.0; t < cycles*2*math.Pi; t += res {
+			x := math.Sin(t)
+			y := math.Sin(t*freq + phase)
+			// (x, y)の位置の色を変更する、白、赤、青、黄からランダムに色を選択する
+			img.SetColorIndex(size+int(x*size+0.5), size+int(y*size+0.5), (uint8)((rand.Intn(3))+1))
+		}
+		phase += 0.1
+		anim.Delay = append(anim.Delay, delay)
+		anim.Image = append(anim.Image, img)
+	}
+	gif.EncodeAll(out, &anim)
+}
